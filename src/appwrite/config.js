915f2ca2d641c2033e8da7eb3f6cf72bbfba1dc5
RD <dasrana373@gmail.com>
2026-07-04 21:18:02 +0530

    rana: update

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -81,7 +81,7 @@ export class Service{
         }
     }
 
-    async getPosts(queries = [Query.equal("Status ", "Active")]){
+    async getPosts(queries = [Query.equal("status", "active")]){
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabasetId,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
 import {Container, PostCard} from '../components'
 
 function Home() {
     const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         appwriteService.getPosts().then((posts) => {
             if (posts) {
                 setPosts(posts.documents)
+                setLoading(false)
+            }
+            else {
+                setLoading(false)
             }
         })
     }, [])
-  
+
+    if(loading) { 
+        return <>loading...</>
+    }
     if (posts.length === 0) {
         return (
             <div className="w-full py-8 mt-4 text-center">
